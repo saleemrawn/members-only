a0da572e109a05b2d329e08aa60c3df364021679
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-07 16:47:51 +0100

    fix: use app.use() instead of app.set() for urlencoded middleware
    - resolve issue with req.body returning undefined

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set(express.urlencoded({ extended: true }));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
 app.use("/", indexRouter);
 
