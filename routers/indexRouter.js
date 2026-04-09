abb6c81705ce1e9a11c104a41c4fedf6cdfaed05
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-09 09:08:18 +0100

    feat: authenticate login POST with passport.authenticate

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const indexController = require("../controllers/indexController");
+const passport = require("passport");
 const indexRouter = Router();
 
 indexRouter.get("/", indexController.getAllMessages);
@@ -7,5 +8,6 @@ indexRouter.get("/login", indexController.getLogin);
 indexRouter.get("/sign-up", indexController.getCreateMember);
 
 indexRouter.post("/sign-up", indexController.memberValidators, indexController.createMember);
+indexRouter.post("/login", passport.authenticate("local", { successRedirect: "/", failureRedirect: "/login" }));
 
 module.exports = indexRouter;
