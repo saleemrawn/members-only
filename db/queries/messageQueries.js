c48904a6f823b62bcfbac5d248b0e614f84ba162
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-04-10 20:18:25 +0100

    refactor: update getAllMessages by created_at descending order

--- db/queries/messageQueries.js
+++ db/queries/messageQueries.js
@@ -4,7 +4,8 @@ async function getAllMessages() {
   const { rows } = await pool.query(`
     SELECT me.id, me.message, me.author_id, CONCAT(us.first_name, ' ', us.last_name) AS author_name, me.created_at
     FROM messages AS me
-    LEFT JOIN users AS us ON me.author_id = us.id;`);
+    LEFT JOIN users AS us ON me.author_id = us.id
+    ORDER BY me.created_at DESC;`);
 
   return rows;
 }
